3c89772430ded048cc85ee09649a54b603ec5b6b
IjaasMohamed <undyin0@gmail.com>
2023-03-08 08:00:43 -0300

    form capture and get value

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -6,10 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss'],
 })
 export class EmpAddEditComponent {
+  empForm: FormGroup;
+
   employeeType: String[] = [
     'Full-Time',
     'Contract-Basis',
     'Part-Time',
     'Other',
   ];
+
+  constructor(private _fb: FormBuilder) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      employeeType: '',
+      designation: '',
+      yearsOfExperience: '',
+      expectedSalary: '',
+    });
+  }
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      console.log(this.empForm.value);
+    }
+  }
 }
